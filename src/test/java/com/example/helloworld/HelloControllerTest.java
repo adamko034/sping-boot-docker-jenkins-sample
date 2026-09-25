eab0ed0e214a0432324cde117c67dd25d7c9f9cc
adamko <adamko034@outlook.com>
2026-09-25 16:41:22 +0200

    fix test

--- src/test/java/com/example/helloworld/HelloControllerTest.java
+++ src/test/java/com/example/helloworld/HelloControllerTest.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -10,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HelloController.class)
+@TestPropertySource(properties = "greeting.name=Adam")
 class HelloControllerTest {
 
     @Autowired
@@ -19,6 +21,6 @@ class HelloControllerTest {
     void helloReturnsHelloAdam() throws Exception {
         mockMvc.perform(get("/api/hello"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello World"));
+                .andExpect(content().string("Hello Adam"));
     }
 }
